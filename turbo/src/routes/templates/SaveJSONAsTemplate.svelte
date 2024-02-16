358bd8c163ea5d8a7c467554a1769d45690ca239
Orb <admin@lnorb.com>
2024-02-16 06:51:49 +0000

    readme updated, template management, git hooks are now optional
    - Templates can now be copied and added using the /templates path
    - Git hooks are now optional via the `TTTC_GIT_HOOKS` environment variable
    - Refined readme for fresh deployments

---
+++ turbo/src/routes/templates/SaveJSONAsTemplate.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Button, Dialog, TextField } from 'svelte-ux';
+	export let open = false;
+	import { saveTemplate } from '$lib/templates';
+
+	let name = '';
+	let data = '';
+</script>
+
+<Dialog {open}>
+	<div slot="title">Save JSON as template</div>
+	<div class="p-2">
+		<TextField bind:value={name} label="template name" autofocus />
+		{name}
+	</div>
+	<div class="p-2">
+		<TextField bind:value={data} label="template JSON body" />
+	</div>
+	<div slot="actions">
+		<Button
+			variant="fill"
+			color="primary"
+			on:click={() => {
+				saveTemplate(name, JSON.parse(data));
+			}}>OK</Button
+		>
+		<Button
+			on:click={() => {
+				open = false;
+			}}>Cancel</Button
+		>
+	</div>
+</Dialog>
